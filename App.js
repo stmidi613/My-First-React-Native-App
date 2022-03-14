98f543eaa3cdad5eaa8d638d23c5726c6ea476eb
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2022-03-14 14:44:23 +0900

    I did more of the tutorial and finished a counter project.

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [click, setClick] = useState(0);
+
+  const onclickhandler = () => {
+  setCount(() => count + 5);
+  setClick(() => click + 1);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>This is my first app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>{count}</Text>
+      <Button style={styles.button} title="Add" onPress={onclickhandler}></Button>
+      <Text style={styles.text}>You clicked {click} times!</Text>
     </View>
   );
 }
@@ -13,8 +22,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ff0',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    color: 'white',
+    borderRadius: 5,
+    backgroundColor: 'lightblue'
+  },
+  text: {
+    color: "white"
+  }
+  
 });
